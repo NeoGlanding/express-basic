f0aa194baa3f6769b255ab8a6a5f9e2cb2c763d3
ihsanabdrhmn <askingbraving@protonmail.com>
2021-08-29 13:11:50 +0700

    post request to fake api

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-
 const mentors = JSON.parse(fs.readFileSync(`${__dirname}/data/data.json`));
+app.use(express.json());
 
 app.get('/api/v1/mentors', (req, res) => {
     res.status(200).json({
@@ -14,9 +14,17 @@ app.get('/api/v1/mentors', (req, res) => {
     })
 });
 
-app.post('/', (req, res) => {
+app.post('/api/v1/mentors', (req, res) => {
+    const newObject = {...req.body, id: mentors.length + 1};
+
+    mentors.push(newObject);
+    fs.writeFile(`${__dirname}/data/data.json`, JSON.stringify(mentors), () => {
+        console.log('success');
+    });
+
     res.status(201).json({
-        message: 'post is finished'
+        message: 'success',
+        data: newObject
     })
 })
 
