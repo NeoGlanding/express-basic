236ced35f6c51b3484b2d414b4cba2e53443a4ef
NeoGlanding <neoglanding@protonmail.com>
2021-08-29 17:12:02 +0700

    routing

--- app.js
+++ app.js
@@ -5,18 +5,16 @@ const app = express();
 const mentors = JSON.parse(fs.readFileSync(`${__dirname}/data/data.json`));
 app.use(express.json());
 
-app.get('/api/v1/mentors', (req, res) => {
+const getAllMentors = (req, res) => {
     res.status(200).json({
         message: 'success',
         response: {
             mentors
         }
     })
-});
+}
 
-
-
-app.post('/api/v1/mentors', (req, res) => {
+const postMentor = (req, res) => {
     const newObject = {...req.body, id: mentors.length + 1};
 
     mentors.push(newObject);
@@ -28,7 +26,40 @@ app.post('/api/v1/mentors', (req, res) => {
         message: 'success',
         data: newObject
     })
-})
+}
+const getSpecificMentor = (req, res) => {
+    const mentor = mentors.find(el => el.name === req.params.name);
+    if (!mentor) {
+        return res.status(404).json({
+            status: 'failed',
+            message: 'Not found'
+        });
+    }
+    
+    res.status(200).json({
+        status: 'success',
+        data: mentor
+    });
+}
+
+const patchMentor = (req, res) => {
+    console.log(req.body);
+    const mentor = mentors.find(el => el.name === req.params.name);
+    if (!mentor) {
+        console.log('Not found');
+    } 
+    else {
+        const newObj = {...mentor, ...req.body}
+        fs.writeFile(`${__dirname}/data/data.json`, JSON.stringify(newObj), () => {
+            console.log('success');
+        });
+    }
+}
+
+app.get('/api/v1/mentors', getAllMentors);
+app.post('/api/v1/mentors', postMentor);
+app.get('/api/v1/mentors/:name', getSpecificMentor);
+app.patch('/api/v1/mentors/:name', patchMentor);
 
 const port = 3000;
 app.listen(port, () => {
